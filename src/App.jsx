d44f835ec86b22f415d7e54946560e85f7e96bf2
Ren Yamazaki <r26763041@gmail.com>
2025-06-27 03:39:43 +0900

    HTML/CSS部分の作成

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,50 @@
 import "./App.css";
 
 function App() {
-  return <>Hello World!</>;
+  return (
+    <>
+      <div className="input-area">
+        <input placeholder="ToDoを入力" />
+        <button>追加</button>
+      </div>
+      <div className="incomplete-area">
+        <p className="title">未完了のToDo</p>
+        <ul>
+          <li>
+            <div className="list-row">
+              <p className="todo-item">ToDoです</p>
+              <button>完了</button>
+              <button>削除</button>
+            </div>
+          </li>
+          <li>
+            <div className="list-row">
+              <p className="todo-item">ToDoです</p>
+              <button>完了</button>
+              <button>削除</button>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <div className="complete-area">
+        <p className="title">完了のToDo</p>
+        <ul>
+          <li>
+            <div className="list-row">
+              <p className="todo-item">ToDoでした</p>
+              <button>戻す</button>
+            </div>
+          </li>
+          <li>
+            <div className="list-row">
+              <p className="todo-item">ToDoでした</p>
+              <button>戻す</button>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
 }
 
 export default App;
