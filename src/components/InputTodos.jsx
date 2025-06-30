75cff600d935b29959e5989635dfc033ed36de5e
Ren Yamazaki <r26763041@gmail.com>
2025-07-01 02:04:57 +0900

    CSSもコンポーネント内に記述

--- src/components/InputTodos.jsx
+++ src/components/InputTodos.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 
+const style = {
+  backgroundColor: "#c6e5d9",
+  width: "400px",
+  height: "30px",
+  padding: "8px",
+  margin: "8px",
+  borderRadius: "8px",
+};
+
 const InputTodos = (props) => {
   const { todoText, onChangeTodoText, onClickAdd } = props;
-  
+
   return (
-    <div className="input-area">
+    <div style={style}>
       <input
         placeholder="ToDoを入力"
         value={todoText}
